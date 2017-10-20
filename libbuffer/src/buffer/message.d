d7ca418060c7038be76c9baf7d78404068b6328d
shove <shove@localhost>
2017-10-20 21:44:27 +0800

    Add RPC.

--- libbuffer/src/buffer/message.d
+++ libbuffer/src/buffer/message.d
@@ -1,53 +1,14 @@
 module buffer.message;
 
 import std.traits;
-import std.conv : to;
-import std.bitmanip;
 import std.typecons;
 import std.variant;
+import std.conv : to;
 
-import cryption.tea.xtea;
-import cryption.aes;
 import cryption.rsa;
 
 public import buffer.compiler;
-import buffer.utils;
-
-enum CryptType
-{
-    NONE = 0,
-    XTEA = 1,
-    AES =  2,
-    RSA =  3
-}
-
-template TypeID(Type)
-{
-         static if (is(Unqual!Type == byte))
-        const ubyte TypeID = 0x01;
-    else static if (is(Unqual!Type == ubyte))
-        const ubyte TypeID = 0x02;
-    else static if (is(Unqual!Type == short))
-        const ubyte TypeID = 0x03;
-    else static if (is(Unqual!Type == ushort))
-        const ubyte TypeID = 0x04;
-    else static if (is(Unqual!Type == int))
-        const ubyte TypeID = 0x05;
-    else static if (is(Unqual!Type == uint))
-        const ubyte TypeID = 0x06;
-    else static if (is(Unqual!Type == long))
-        const ubyte TypeID = 0x07;
-    else static if (is(Unqual!Type == ulong))
-        const ubyte TypeID = 0x08;
-    else static if (is(Unqual!Type == float))
-        const ubyte TypeID = 0x20;
-    else static if (is(Unqual!Type == double))
-        const ubyte TypeID = 0x21;
-    else static if (is(Unqual!Type == string))
-        const ubyte TypeID = 0x50;
-    else
-        static assert(0, "Data types that are not supported: " ~ typeid(Type));
-}
+public import buffer.packet;
 
 abstract class Message
 {
@@ -94,128 +55,21 @@ public:
 
     static void getMessageInfo(ubyte[] buffer, out ushort messageId, out TypeInfo_Class messageClass, out string method)
     {
-        assert(buffer != null && buffer.length >= 12, "Incorrect buffer length.");
-
-        messageId = buffer.peek!ushort(6);
+        Packet.parseInfo(buffer, messageId, method);
+        
         if (messageId in _messages)
         {
             messageClass = _messages[messageId];
-        }
-
-        ushort t_method_len = buffer.peek!ushort(8);
-        if (t_method_len > 0)
-        {
-            method = cast(string) buffer[10 .. 10 + t_method_len];
         }
     }
 
     static Variant[] deserialize(ubyte[] buffer, out ushort messageId, out TypeInfo_Class messageClass, out string method)
     {
-        assert(buffer != null && buffer.length >= 12, "Incorrect buffer length.");
+        Variant[] ret = Packet.parse(buffer, Message._magic, Message._crypt, Message._key, Message._rsaKey, messageId, method);
 
-        ushort t_magic, t_crc;
-        int t_len;
-        t_magic = buffer.peek!ushort(0);
-        t_len = buffer.peek!int(2);
-
-        if ((t_magic != Message._magic) || (t_len > buffer.length - 6))
-            return null;
-
-        buffer = buffer[0 .. t_len + 6];
-        t_crc = buffer.peek!ushort(buffer.length - 2);
-        if (strToByte_hex(MD5(buffer[0 .. $ - 2])[0 .. 4]) != buffer[$ - 2 .. $])
-            return null;
-
-        getMessageInfo(buffer, messageId, messageClass, method);
-
-        ushort t_method_len = buffer.peek!ushort(8);
-        buffer = buffer[10 + t_method_len .. $ - 2];
-
-        final switch (Message._crypt)
+        if (messageId in _messages)
         {
-        case CryptType.NONE:
-            break;
-        case CryptType.XTEA:
-            buffer = Xtea.decrypt(buffer, Message._key);
-            break;
-        case CryptType.AES:
-            buffer = AESUtils.decrypt!AES128(buffer, Message._key);
-            break;
-        case CryptType.RSA:
-            buffer = RSA.decrypt(Message._rsaKey, buffer);
-            break;
-        }
-
-        ubyte typeId;
-        int pos;
-        Variant[] ret;
-
-        while (pos < (buffer.length - 1))
-        {
-            typeId = buffer[pos];
-            pos++;
-
-            if (typeId == TypeID!byte)
-            {
-                ret ~= Variant(buffer.peek!byte(pos));
-                pos += byte.sizeof;
-            }
-            else if (typeId == TypeID!ubyte)
-            {
-                ret ~= Variant(buffer.peek!ubyte(pos));
-                pos += ubyte.sizeof;
-            }
-            else if (typeId == TypeID!short)
-            {
-                ret ~= Variant(buffer.peek!short(pos));
-                pos += short.sizeof;
-            }
-            else if (typeId == TypeID!ushort)
-            {
-                ret ~= Variant(buffer.peek!ushort(pos));
-                pos += ushort.sizeof;
-            }
-            else if (typeId == TypeID!int)
-            {
-                ret ~= Variant(buffer.peek!int(pos));
-                pos += int.sizeof;
-            }
-            else if (typeId == TypeID!uint)
-            {
-                ret ~= Variant(buffer.peek!uint(pos));
-                pos += uint.sizeof;
-            }
-            else if (typeId == TypeID!long)
-            {
-                ret ~= Variant(buffer.peek!long(pos));
-                pos += long.sizeof;
-            }
-            else if (typeId == TypeID!ulong)
-            {
-                ret ~= Variant(buffer.peek!ulong(pos));
-                pos += ulong.sizeof;
-            }
-            else if (typeId == TypeID!float)
-            {
-                ret ~= Variant(buffer.peek!float(pos));
-                pos += float.sizeof;
-            }
-            else if (typeId == TypeID!double)
-            {
-                ret ~= Variant(buffer.peek!double(pos));
-                pos += double.sizeof;
-            }
-            else if (typeId == TypeID!string)
-            {
-                int temp = buffer.peek!int(pos);
-                pos += 4;
-                ret ~= Variant(cast(string) buffer[pos .. pos + temp]);
-                pos += temp;
-            }
-            else
-            {
-                assert(0, "Data types id that are not supported: " ~ typeId.to!string);
-            }
+            messageClass = _messages[messageId];
         }
 
         return ret;
@@ -253,147 +107,24 @@ public:
         return message;
     }
 
-    /*
-    static T deserialize(T)(ubyte[] buffer, ref string method)
-    {
-        assert(buffer != null && buffer.length >= 12, "Incorrect buffer length.");
-
-        ushort t_magic, t_crc;
-        int t_len;
-        t_magic = buffer.peek!ushort(0);
-        t_len = buffer.peek!int(2);
-
-        if ((t_magic != Message._magic) || (t_len > buffer.length - 6))
-            return null;
-
-        buffer = buffer[0 .. t_len + 6];
-        t_crc = buffer.peek!ushort(buffer.length - 2);
-        if (strToByte_hex(MD5(buffer[0 .. $ - 2])[0 .. 4]) != buffer[$ - 2 .. $])
-            return null;
-
-        ushort t_messageId = buffer.peek!ushort(6);
-
-        T message = new T();
-        if (message.messageId != t_messageId)
-        {
-            assert(0, "The type T(" ~ T.classinfo.name ~ ") of the incoming template is incorrect. It should be " ~ _messages[t_messageId].name);
-        }
-
-        ushort t_method_len = buffer.peek!ushort(8);
-        method = cast(string) buffer[10 .. 10 + t_method_len];
-        buffer = buffer[10 + t_method_len .. $ - 2];
-
-        final switch (Message._crypt)
-        {
-        case CryptType.NONE:
-            break;
-        case CryptType.XTEA:
-            buffer = Xtea.decrypt(buffer, Message._key);
-            break;
-        case CryptType.AES:
-            buffer = AESUtils.decrypt!AES128(buffer, Message._key);
-            break;
-        case CryptType.RSA:
-            buffer = RSA.decrypt(Message._rsaKey, buffer);
-            break;
-        }
-
-        ubyte typeId;
-        int temp, pos;
-
-        foreach (i, type; FieldTypeTuple!(T))
-        {
-            static if (is(Unqual!type == string))
-            {
-                mixin("
-                    typeId = buffer[pos];
-                    assert(" ~ TypeID!type.to!string ~ " == typeId, \"Data type mismatch.\");
-                    pos++;
-                    temp = buffer.peek!int(pos);
-                    pos += 4;
-                    message." ~ FieldNameTuple!T[i] ~ " = cast(string)buffer[pos..pos + temp];
-                    pos += temp;
-                ");
-            }
-            else
-            {
-                mixin("
-                    typeId = buffer[pos];
-                    assert(" ~ TypeID!type.to!string ~ " == typeId, \"Data type mismatch.\");
-                    pos++;
-                    message." ~ FieldNameTuple!T[i] ~ " = buffer.peek!" ~ type.stringof ~ "(pos);
-                    pos += " ~ type.sizeof.to!string ~ ";
-                ");
-            }
-        }
-
-        return message;
-    }
-    */
 protected:
 
     ubyte[] serialize(T)(T message, string method = string.init)
     {
         assert(message !is null, "The object to serialize cannot be null.");
-        assert(method.length <= 255, "Paramter method cannot be greater than 255 characters.");
 
-        ubyte[] temp1, temp2;
-        ubyte[] tlv;
+        Variant[] params;
 
         foreach (i, type; FieldTypeTuple!T)
         {
-            static if (is(Unqual!type == string))
-            {
-                mixin("
-                    temp1 = new ubyte[4];
-                    temp2 = cast(ubyte[])message." ~ FieldNameTuple!T[i] ~ ";
-                    temp1.write!int(cast(int)temp2.length, 0);
-                    tlv ~= cast(ubyte)" ~ TypeID!type.to!string ~ ";
-                    tlv ~= temp1;
-                    tlv ~= temp2;
-                ");
-            }
-            else
-            {
-                mixin("
-                    temp1 = new ubyte[" ~ type.sizeof.to!string ~ "];
-                    temp1.write!" ~ type.stringof ~ "(message." ~ FieldNameTuple!T[i] ~ ", 0);
-                    tlv ~= cast(ubyte)" ~ TypeID!type.to!string ~ ";
-                    tlv ~= temp1;
-                ");
-            }
+            mixin("
+                params ~= Variant(message." ~ FieldNameTuple!T[i] ~ ");
+            ");
         }
 
-        final switch (Message._crypt)
-        {
-        case CryptType.NONE:
-            break;
-        case CryptType.XTEA:
-            tlv = Xtea.encrypt(tlv, Message._key);
-            break;
-        case CryptType.AES:
-            tlv = AESUtils.encrypt!AES128(tlv, Message._key);
-            break;
-        case CryptType.RSA:
-            tlv = RSA.encrypt(Message._rsaKey, tlv);
-            break;
-        }
-
-        ubyte[] method_buf = cast(ubyte[]) method;
-        ubyte[] buffer = new ubyte[10];
-
-        buffer.write!ushort(Message._magic, 0);
-        buffer.write!int(cast(int)(2 + 2 + method_buf.length + tlv.length + 2), 2);
-        buffer.write!ushort(messageId, 6);
-        buffer.write!ushort(cast(ushort) method_buf.length, 8);
-        if (method_buf.length > 0)
-            buffer ~= method_buf;
-        buffer ~= tlv;
-        buffer ~= strToByte_hex(MD5(buffer)[0 .. 4]);
-
-        return buffer;
+        return Packet.build(Message._magic, Message._crypt, Message._key, Message._rsaKey, message.messageId, method, params);
     }
-
+    
     ushort _messageId;
     __gshared static TypeInfo_Class[ushort] _messages;
 
---
+++ libbuffer/src/buffer/packet.d
@@ -0,0 +1,277 @@
+module buffer.packet;
+
+import std.variant;
+import std.bitmanip;
+import std.traits;
+import std.typecons;
+import std.conv : to;
+
+import cryption.aes;
+import cryption.tea.xtea;
+import cryption.rsa;
+
+import buffer.utils;
+
+enum CryptType
+{
+    NONE = 0,
+    XTEA = 1,
+    AES  = 2,
+    RSA  = 3
+}
+
+template TypeID(Type)
+{
+         static if (is(Unqual!Type == byte))
+        const ubyte TypeID = 0x01;
+    else static if (is(Unqual!Type == ubyte))
+        const ubyte TypeID = 0x02;
+    else static if (is(Unqual!Type == short))
+        const ubyte TypeID = 0x03;
+    else static if (is(Unqual!Type == ushort))
+        const ubyte TypeID = 0x04;
+    else static if (is(Unqual!Type == int))
+        const ubyte TypeID = 0x05;
+    else static if (is(Unqual!Type == uint))
+        const ubyte TypeID = 0x06;
+    else static if (is(Unqual!Type == long))
+        const ubyte TypeID = 0x07;
+    else static if (is(Unqual!Type == ulong))
+        const ubyte TypeID = 0x08;
+    else static if (is(Unqual!Type == float))
+        const ubyte TypeID = 0x20;
+    else static if (is(Unqual!Type == double))
+        const ubyte TypeID = 0x21;
+    else static if (is(Unqual!Type == string))
+        const ubyte TypeID = 0x50;
+    else
+        static assert(0, "Data types that are not supported: " ~ typeid(Type));
+}
+
+package class Packet
+{
+    static ubyte[] build(ushort magic, CryptType crypt, string key, Nullable!RSAKeyInfo rsaKey, ushort messageId, string method, Variant[] params)
+    {
+        assert(params.length > 0, "Parameter params must be provided.");
+        assert(method.length <= 255, "Paramter method cannot be greater than 255 characters.");
+
+        ubyte[] temp;
+        ubyte[] tlv;
+
+        void put(T)(Variant v)
+        {
+            tlv ~= TypeID!T;
+            temp = new ubyte[T.sizeof];
+            temp.write!T(v.get!T, 0);
+            tlv ~= temp;
+        }
+
+        foreach (v; params)
+        {
+            if (v.type == typeid(byte))
+            {
+                put!byte(v);
+            }
+            else if (v.type == typeid(ubyte))
+            {
+                put!ubyte(v);
+            }
+            else if (v.type == typeid(short))
+            {
+                put!short(v);
+            }
+            else if (v.type == typeid(ushort))
+            {
+                put!ushort(v);
+            }
+            else if (v.type == typeid(int))
+            {
+                put!int(v);
+            }
+            else if (v.type == typeid(uint))
+            {
+                put!uint(v);
+            }
+            else if (v.type == typeid(long))
+            {
+                put!long(v);
+            }
+            else if (v.type == typeid(ulong))
+            {
+                put!ulong(v);
+            }
+            else if (v.type == typeid(float))
+            {
+                put!float(v);
+            }
+            else if (v.type == typeid(double))
+            {
+                put!double(v);
+            }
+            else if (v.type == typeid(string))
+            {
+                tlv ~= TypeID!string;
+                string str = v.get!string;
+                temp = new ubyte[4];
+                temp.write!int(cast(int) str.length, 0);
+                tlv ~= temp;
+                temp = cast(ubyte[]) str;
+                tlv ~= temp;
+            }
+            else
+            {
+                assert(0, "Data types id that are not supported: " ~ v.type.toString);
+            }
+        }
+
+        final switch (crypt)
+        {
+        case CryptType.NONE:
+            break;
+        case CryptType.XTEA:
+            tlv = Xtea.encrypt(tlv, key);
+            break;
+        case CryptType.AES:
+            tlv = AESUtils.encrypt!AES128(tlv, key);
+            break;
+        case CryptType.RSA:
+            tlv = RSA.encrypt(rsaKey, tlv);
+            break;
+        }
+
+        ubyte[] method_buf = cast(ubyte[]) method;
+        ubyte[] buffer = new ubyte[10];
+
+        buffer.write!ushort(magic, 0);
+        buffer.write!int(cast(int)(2 + 2 + method_buf.length + tlv.length + 2), 2);
+        buffer.write!ushort(messageId, 6);
+        buffer.write!ushort(cast(ushort) method_buf.length, 8);
+        if (method_buf.length > 0)
+            buffer ~= method_buf;
+        buffer ~= tlv;
+        buffer ~= strToByte_hex(MD5(buffer)[0 .. 4]);
+
+        return buffer;
+    }
+
+    static void parseInfo(ubyte[] buffer, out ushort messageId, out string method)
+    {
+        assert(buffer != null && buffer.length >= 12, "Incorrect buffer length.");
+
+        messageId = buffer.peek!ushort(6);
+
+        ushort t_method_len = buffer.peek!ushort(8);
+        if (t_method_len > 0)
+        {
+            method = cast(string) buffer[10 .. 10 + t_method_len];
+        }
+    }
+
+    static Variant[] parse(ubyte[] buffer, ushort magic, CryptType crypt, string key, Nullable!RSAKeyInfo rsaKey, out ushort messageId, out string method)
+    {
+        assert(buffer != null && buffer.length >= 12, "Incorrect buffer length.");
+
+        ushort t_magic, t_crc;
+        int t_len;
+        t_magic = buffer.peek!ushort(0);
+        t_len = buffer.peek!int(2);
+
+        if ((t_magic != magic) || (t_len > buffer.length - 6))
+            return null;
+
+        buffer = buffer[0 .. t_len + 6];
+        t_crc = buffer.peek!ushort(buffer.length - 2);
+        if (strToByte_hex(MD5(buffer[0 .. $ - 2])[0 .. 4]) != buffer[$ - 2 .. $])
+            return null;
+
+        parseInfo(buffer, messageId, method);
+
+        ushort t_method_len = buffer.peek!ushort(8);
+        buffer = buffer[10 + t_method_len .. $ - 2];
+
+        final switch (crypt)
+        {
+        case CryptType.NONE:
+            break;
+        case CryptType.XTEA:
+            buffer = Xtea.decrypt(buffer, key);
+            break;
+        case CryptType.AES:
+            buffer = AESUtils.decrypt!AES128(buffer, key);
+            break;
+        case CryptType.RSA:
+            buffer = RSA.decrypt(rsaKey, buffer);
+            break;
+        }
+
+        ubyte typeId;
+        int pos;
+        Variant[] ret;
+
+        void get(T)()
+        {
+            ret ~= Variant(buffer.peek!T(pos));
+            pos += T.sizeof;
+        }
+
+        while (pos < buffer.length)
+        {
+            typeId = buffer[pos];
+            pos++;
+
+            if (typeId == TypeID!byte)
+            {
+                get!byte;
+            }
+            else if (typeId == TypeID!ubyte)
+            {
+                get!ubyte;
+            }
+            else if (typeId == TypeID!short)
+            {
+                get!short;
+            }
+            else if (typeId == TypeID!ushort)
+            {
+                get!ushort;
+            }
+            else if (typeId == TypeID!int)
+            {
+                get!int;
+            }
+            else if (typeId == TypeID!uint)
+            {
+                get!uint;
+            }
+            else if (typeId == TypeID!long)
+            {
+                get!long;
+            }
+            else if (typeId == TypeID!ulong)
+            {
+                get!ulong;
+            }
+            else if (typeId == TypeID!float)
+            {
+                get!float;
+            }
+            else if (typeId == TypeID!double)
+            {
+                get!double;
+            }
+            else if (typeId == TypeID!string)
+            {
+                int temp = buffer.peek!int(pos);
+                pos += 4;
+                ret ~= Variant(cast(string) buffer[pos .. pos + temp]);
+                pos += temp;
+            }
+            else
+            {
+                assert(0, "Data types id that are not supported: " ~ typeId.to!string);
+            }
+        }
+
+        return ret;
+    }
+}
