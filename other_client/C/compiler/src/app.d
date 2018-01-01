faae2a075db46e5a3e6585657139c8d23aee8f67
shove70 <shove@163.com>
2018-01-01 18:01:00 +0800

    Update the buffc compiler.

--- other_client/C/compiler/src/app.d
+++ other_client/C/compiler/src/app.d
@@ -1,70 +1,113 @@
 import std.array;
 import std.stdio;
 import std.file;
+import std.path;
+import std.algorithm;
 import std.conv;
 import std.string;
+import std.uni;
 
 import buffer.compiler;
 
 void main(string[] args)
 {
-    if (args.length < 3)
+    if ((args.length < 3) || ((args[1] == "-c++11") && (args.length < 4)))
     {
-        writeln("Usage: ./buffc input_filename(.buffer) output_filename(.h)");
+        writeln("Usage: ./buffc [-c++11] src_path dst_path");
         return;
     }
 
-    if (!std.file.exists(args[1]))
-    {
-        writeln("The input file " ~ args[1] ~ " not exists.");
-        return;
-    }
-
-    Token[] tokens = lexer(std.file.readText(args[1]));
-    Sentence[] sentences = parser(tokens);
+    bool cpp11 = ((args[1] == "-c++11") || (args[1] == "-cpp11"));
     
-    Appender!string code;
-    code.put("#pragma once\r\n\r\n");
-    code.put("#include <vector>\r\n");
-    code.put("#include \"{Replace the real path}/message.h\"\r\n\r\n");
-    code.put("using namespace std;\r\n");
-    code.put("using namespace buffer;\r\n\r\n");
+    string src = args[1 + (cpp11 ? 1 : 0)];
+    string dst = args[2 + (cpp11 ? 1 : 0)];
 
-    foreach (sentence; sentences)
+    if (!std.file.exists(src))
     {
-        code.put("class " ~ sentence.name ~ " : Message\r\n");
-        code.put("{\r\n");
-        code.put("public:\r\n");
-        code.put("\tstring _className = \"" ~ sentence.name ~ "\";\r\n\r\n");
-
-        foreach (field; sentence.fields)
-        {
-            code.put("\t" ~ field.type ~ " " ~ field.name ~ ";\r\n");
-        }
-
-        code.put("\r\n");
-        code.put("\tvoid setValue(vector<Any>& params)\r\n");
-        code.put("\t{\r\n");
-
-        foreach (i, field; sentence.fields)
-        {
-            code.put("\t\t" ~ field.name ~ " = params[" ~ i.to!string ~ "].cast<" ~ field.type ~ ">();\r\n");
-        }
-
-        code.put("\t}\r\n\r\n");
-        code.put("\tvoid serialize(vector<ubyte>& buffer, string method = \"\")\r\n");
-        code.put("\t{\r\n");
-        code.put("\t\tMessage::serialize(buffer, \"" ~ sentence.name ~ "\", method");
-
-        foreach (i, field; sentence.fields)
-        {
-            code.put(", " ~ field.name);
-        }
-
-        code.put(");\r\n");
-        code.put("\t}\r\n");
-        code.put("};\r\n\r\n");
+        writeln("The src path" ~ src ~ " not exists.");
+        return;
+    }
+    
+    if (std.file.isFile(src))
+    {
+        writeln("The src path" ~ src ~ " is a file, not a path.");
+        return;
     }
 
-    std.file.write(args[2], cast(ubyte[])code.data);
+    if (std.file.exists(dst) && std.file.isFile(dst))
+    {
+        writeln("The dst path" ~ src ~ " is a file, not a path.");
+        return;
+    }
+
+    std.file.mkdirRecurse(dst);
+    
+    foreach (DirEntry e; dirEntries(src, SpanMode.shallow).filter!(a => a.isFile))
+    {
+        string srcFile = e.name;
+        if (toLower(std.path.extension(srcFile)) != ".buffer")
+        {
+            continue;
+        }
+        string fileName = baseName(e.name, ".buffer");
+        string dstFilename = buildPath(dst, fileName ~ ".h");
+
+        Token[] tokens = lexer(std.file.readText(e.name));
+        Sentence[] sentences = parser(tokens);
+        
+        Appender!string code;
+        code.put("#pragma once\r\n\r\n");
+        code.put("#include <vector>\r\n");
+        code.put("#include \"{ Replace the real path }/message.h\"\r\n\r\n");
+        code.put("using namespace std;\r\n");
+        code.put("using namespace buffer;\r\n\r\n");
+    
+        foreach (sentence; sentences)
+        {
+            code.put("class " ~ sentence.name ~ " : Message\r\n");
+            code.put("{\r\n");
+            code.put("public:\r\n");
+            if (cpp11)
+            {
+                code.put("\tstring _className = \"" ~ sentence.name ~ "\";\r\n\r\n");
+            }
+            else
+            {
+                code.put("\tstring _className()\r\n");
+                code.put("\t{\r\n");
+                code.put("\t\treturn \"" ~ sentence.name ~ "\";\r\n");
+                code.put("\t}\r\n\r\n");
+            }
+
+            foreach (field; sentence.fields)
+            {
+                code.put("\t" ~ field.type ~ " " ~ field.name ~ ";\r\n");
+            }
+    
+            code.put("\r\n");
+            code.put("\tvoid setValue(vector<Any>& params)\r\n");
+            code.put("\t{\r\n");
+    
+            foreach (i, field; sentence.fields)
+            {
+                code.put("\t\t" ~ field.name ~ " = params[" ~ i.to!string ~ "].cast<" ~ field.type ~ ">();\r\n");
+            }
+    
+            code.put("\t}\r\n\r\n");
+            code.put("\tvoid serialize(vector<ubyte>& buffer, string method = \"\")\r\n");
+            code.put("\t{\r\n");
+            code.put("\t\tMessage::serialize(buffer, \"" ~ sentence.name ~ "\", method");
+    
+            foreach (i, field; sentence.fields)
+            {
+                code.put(", " ~ field.name);
+            }
+    
+            code.put(");\r\n");
+            code.put("\t}\r\n");
+            code.put("};\r\n\r\n");
+        }
+    
+        std.file.write(dstFilename, cast(ubyte[])code.data);
+    }
 }
